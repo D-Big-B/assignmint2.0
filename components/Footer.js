21251484f4587f32a9139152a1df95350dc1b645
D-Big-B <prjjwlbhatt@gmail.com>
2023-06-03 16:23:15 +0530

    Footer added .

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,15 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <Grid container className={styles.container}></Grid>;
+  return (
+    <div className={styles.container}>
+      <p>Copyright © 2023 All rights reserved by</p>
+
+      <p>
+        <span>Assignmint</span>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ const Layout = (props) => {
       <MainNavbar />
 
       {props.children}
+      <Footer />
     </>
   );
 };
